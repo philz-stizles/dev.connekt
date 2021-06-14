8b83099edbad38ea40afb4e64f44a1c1bd2723c7
philz-stizles <theophilusighalo@gmail.com>
2021-06-14 19:02:31 +0100

    Add console log

--- db/index.js
+++ db/index.js
@@ -3,21 +3,23 @@ const config = require('config');
 
 const db = config.get('mongoURI');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(db, { 
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log('DB Connected ...');
-    } catch (error) {
-        console.error(error.message);
+console.log('MONOGO DB SERVER', db);
 
-        // Exit process with failure
-        process.exit();
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log('DB Connected ...');
+  } catch (error) {
+    console.error(error.message);
+
+    // Exit process with failure
+    process.exit();
+  }
+};
 
 module.exports = connectDB;
