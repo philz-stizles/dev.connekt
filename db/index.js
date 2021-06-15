35010e39213b774d0fb49fb13b4b3ed940d11b2c
philz-stizles <theophilusighalo@gmail.com>
2021-06-15 04:37:50 +0100

    dotenv config

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoURI');
+const db = process.env.mongoURI; // config.get('mongoURI');
 
 console.log('MONOGO SERVER', db);
 
